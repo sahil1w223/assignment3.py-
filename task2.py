dfb5262647236fff8884d9d03092b381eca833a8
sahil1w223 <sahiljal751@gmail.com>
2025-05-17 15:20:51 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -2,6 +2,7 @@ list1 = [1 ,2 ,3, 4, 5, 6, 7, 8, 9, 10]
 list2 = []
 for i in range (len(list1)//2):
     list2.append(list1[i])
+print("Extracted first five elements : ", list2)
 
 
 list3 = []
@@ -9,4 +10,6 @@ for j in range (len(list2)):
     num = 0
     num = num + list2[len(list2)-1-j]
     list3.append(num)
-print(list3)
+print("Reversed extracted list : ", list3)
+
+ 
